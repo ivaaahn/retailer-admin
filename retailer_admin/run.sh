9bd4ae249c153612bb977d5f5ca653e6eeb87a2b
d_ivahnencko <ivahnencko01@gmail.com>
2022-06-06 01:01:23 +0300

    RETAD-1: Update GitHub Actions and poetry init

--- retailer_admin/run.sh
+++ retailer_admin/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate
+sleep 15
 python manage.py createsuperuser --noinput
 uvicorn retailer_admin.asgi:application "--proxy-headers" --host '0.0.0.0' --port '8000' --reload
