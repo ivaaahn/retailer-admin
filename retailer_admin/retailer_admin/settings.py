cc9c4d45a793a879b8f7891b01cc868a0c903bf0
d_ivahnencko <ivahnencko01@gmail.com>
2022-06-10 10:41:33 +0300

    RETAD-2: Optimizing queries to database

--- retailer_admin/retailer_admin/settings.py
+++ retailer_admin/retailer_admin/settings.py
@@ -192,27 +192,28 @@ REDIS_PORT = config["REDIS_PORT"]
 REDIS_DB = config["REDIS_DB"]
 
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
+if config.get("LOGGING", False):
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+        "handlers": {
+            "mail_admins": {
+                "level": "ERROR",
+                "filters": ["require_debug_false"],
+                "class": "django.utils.log.AdminEmailHandler",
+            },
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+            },
         },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+            "django.db.backends": {"handlers": ["console"], "level": "DEBUG"},
         },
-    },
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-        "django.db.backends": {"handlers": ["console"], "level": "DEBUG"},
-    },
-}
+    }
